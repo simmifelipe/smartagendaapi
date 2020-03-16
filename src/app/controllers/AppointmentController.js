ce9c5e910855070b98afab066c80040768b69206
felipe.simmi <simmifelipe1992@gmail.com>
2020-03-16 18:00:31 -0300

    Criada rota de cancelamento de agendamento

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,5 +1,5 @@
 
-import { format, isBefore, parseISO, startOfHour } from 'date-fns';
+import { format, isBefore, parseISO, startOfHour, subHours } from 'date-fns';
 import pt from 'date-fns/locale/pt';
 import * as Yup from 'yup';
 import Appointment from '../models/Appointment';
@@ -100,6 +100,30 @@ class AppointmentController {
       content: `Novo agendamento de ${user.name} para ${formattedDate}`,
       user: provider_id,
     })
+
+    return res.json(appointment);
+  }
+
+  async delete(req, res) {
+
+    const appointment = await Appointment.findByPk(req.params.id);
+
+    if (appointment.user_id !== req.userId) {
+      return res.status(401).json({
+        error: "You don't have permission to cancel this appointment.",
+      });
+    }
+
+    const dateWithSub = subHours(appointment.date, 2);
+
+    if (isBefore(dateWithSub, new Date())) {
+      return res.status(401).json({
+        error: 'You can only appointments 2 houts in advance.',
+      });
+    }
+
+    appointment.canceled_at = new Date().toISOString();
+    await appointment.save();
 
     return res.json(appointment);
   }
