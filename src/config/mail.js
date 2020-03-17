01e46b7d86020e9558889655c43e894fd0f3bba0
Felipe Simmi <simmifelipe1992@gmail.com>
2020-03-17 12:58:11 -0300

    FInalizada primeira versão do backend

---
+++ src/config/mail.js
@@ -0,0 +1,13 @@
+
+export default {
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
+  secure: process.env.MAIL_SECURE,
+  auth: {
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
+  },
+  default: {
+    from: 'Equipe SmartAgenda <noreply@snartagenda.com',
+  },
+};
---
+++ src/config/sentry.js
@@ -0,0 +1,3 @@
+export default {
+  dsn: 'https://e4b6d51f6c78433190cb1a9e09f8fc0b@sentry.io/4971420',
+}
